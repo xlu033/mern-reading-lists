e564d5a72464553d6688c8dea800bc1f1adeb26c
Annie Lu <67603428+xlu033@users.noreply.github.com>
2020-11-13 17:14:40 -0500

    Dev (#3)
    * backend
    
    * book-create and user-create components
    
    * modifications front-end
    
    * modified connection of frontend-backend

--- reading-list/src/App.js
+++ reading-list/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
@@ -21,30 +20,7 @@ function App() {
         <Route path="/user" component={CreateUser} />
       </div>
     </Router>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> 18fc9a7e177b794d61d4635722a27ebc8d8e8443
-  );
+   );
 }
 
 export default App;
--- reading-list/src/components/edit-book.js
+++ reading-list/src/components/edit-book.js
@@ -1,10 +1,191 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 export default class EditBook extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.onChangeUsername = this.onChangeUsername.bind(this);
+      this.onChangeTitle = this.onChangeTitle.bind(this);
+      this.onChangeDescription = this.onChangeDescription.bind(this);
+      this.onChangeAuthor = this.onChangeAuthor.bind(this);
+      this.onChangeLanguage = this.onChangeLanguage.bind(this);
+      this.onChangeDuration = this.onChangeDuration.bind(this);
+      this.onChangeDate = this.onChangeDate.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+  
+      this.state = {
+        username: '',
+        title: '',
+        description: '',
+        author: '',
+        language: '',
+        duration: 0,
+        date: new Date(),
+        users: []
+      }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/books/'+this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          username: this.state.username,
+          title: this.state.title,
+          description: this.state.description,
+          author: this.state.author,
+          language: this.state.language,
+          duration: this.state.duration,
+          date: new Date(response.data.date)
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        this.setState({ users: response.data.map(user => user.username) });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const book = {
+      username: this.state.username,
+      title: this.state.title,
+      description: this.state.description,
+      author: this.state.author,
+      language: this.state.language,
+      duration: this.state.duration,
+      date: this.state.date,
+    };
+
+    console.log(book);
+
+    axios.post('http://localhost:5000/books/update/'+this.props.match.params.id, exercise)
+      .then(res => console.log(res.data));
+    
+    window.location = '/';
+  }
+
   render() {
     return (
       <div>
-        <p>We are on the Edit Book component!</p>
+        <h3>Edit Reading Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <select ref="userInput"
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}>
+                {
+                  this.state.users.map(function(user) {
+                    return <option 
+                      key={user}
+                      value={user}>{user}
+                      </option>;
+                  })
+                }
+            </select>
+          </div>
+
+          <div className="form-group"> 
+              <label>Book title: </label>
+              <input  type="text"
+                  required
+                  className="form-control"
+                  value={this.state.title}
+                  onChange={this.onChangeTitle}
+                  />
+            </div>
+
+            <div className="form-group"> 
+              <label>Description: </label>
+              <input  type="text"
+                  required
+                  className="form-control"
+                  value={this.state.description}
+                  onChange={this.onChangeDescription}
+                  />
+            </div>
+
+            <div className="form-group"> 
+              <label>Author: </label>
+              <input  type="text"
+                  required
+                  className="form-control"
+                  value={this.state.author}
+                  onChange={this.onChangeAuthor}
+                  />
+            </div>
+
+            <div className="form-group"> 
+              <label>Language: </label>
+              <input  type="text"
+                  required
+                  className="form-control"
+                  value={this.state.language}
+                  onChange={this.onChangeLanguage}
+                  />
+            </div>
+
+            <div className="form-group">
+              <label>Duration: </label>
+              <input 
+                  type="text" 
+                  className="form-control"
+                  value={this.state.duration}
+                  onChange={this.onChangeDuration}
+                  />
+            </div>
+
+            <div className="form-group">
+              <label>Date: </label>
+              <div>
+                <DatePicker
+                  selected={this.state.date}
+                  onChange={this.onChangeDate}
+                />
+              </div>
+            </div>
+
+
+          <div className="form-group">
+            <input type="submit" value="Edit Reading Log" className="btn btn-primary" />
+          </div>
+        </form>
       </div>
     )
   }
